c9d2b826a1f076093134575248d2ca5c10defe90
Andbram <70213852+Andbram@users.noreply.github.com>
2021-11-14 15:50:12 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+sudo apt install cmake
+sudo apt install python3-pip
+git clone https://github.com/davisking/dlib.git
+cd dlib
+mkdir build; cd build; cmake ..; cmake --build .
+cd ..
+pip3 install face_recognition
+git clone https://github.com/ageitgey/face_recognition.git
+git clone https://github.com/Andbram/face_rec.git
