b268e95fcba6aa35245ecd19dfec7a3b998e547d
Andbram <70213852+Andbram@users.noreply.github.com>
2021-11-14 15:52:48 -0700

    Add files via upload

--- parse_xml.py
+++ parse_xml.py
@@ -1,7 +1,7 @@
 import re
 
-inpt = "labels_ibug_300W_test.xml"
-outpt = "labels_ibug_300W_test_38point.xml"
+inpt = "labels_ibug_300W_train.xml"
+outpt = "labels_ibug_300W_train_custom.xml"
 
 LANDMARKS = {5, 6, 7, 8, 9, 10, 11, 12, 13, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}
 
