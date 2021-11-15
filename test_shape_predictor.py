fa9bcd61ae7ab137ca53f58346bd4d15e69b159f
Andbram <70213852+Andbram@users.noreply.github.com>
2021-11-14 19:59:10 -0700

    Update test_shape_predictor.py

--- test_shape_predictor.py
+++ test_shape_predictor.py
@@ -1,7 +1,7 @@
 import dlib
 
-model = "38_predictor.dat"
-xml = "labels_ibug_300W_test_38point.xml"
+model = "custom_predictor.dat"
+xml = "labels_ibug_300W_test_custom.xml"
 
 print("[INFO] evaluating shape predictor...")
 error = dlib.test_shape_predictor(xml, model)
